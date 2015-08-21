bf7c4bfcb99fe25f00430db0a3584c05bd085c8f
Martin Jensen <ingvar@netaben.dk>
2015-08-21 14:07:12 +0200

    Adding EnsureHttpsFilter that ensures https scheme is used for all filtered uris

---
+++ src/Nancy.Linker.Tests/EnsureHttpsFilterTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Nancy.Linker.Tests
+{
+  using System;
+  using Testing;
+  using Xunit;
+
+  public class EnsureHttpsFilterr_Should
+  {
+    private readonly Browser app = new Browser(with => with.Module<TestModule>(), defaults: to => to.HostName("nancyfx.org"));
+
+    public class TestModule : NancyModule
+    {
+      public static IResourceLinker linker;
+
+      public TestModule(IResourceLinker linker)
+      {
+        TestModule.linker = linker;
+        this.Get[""] = _ => 200;
+      }
+    }
+
+    [Fact]
+    public void changes_scheme_from_http_to_https()
+    {
+      EnsureHttpsFilter filter = new EnsureHttpsFilter();
+
+      Uri result = filter.Apply(new Uri("http://www.nancyfx.org"), null);
+      Assert.Equal("https", result.Scheme);
+    }
+
+    [Fact]
+    public void handles_mixed_scheme_casing()
+    {
+      EnsureHttpsFilter filter = new EnsureHttpsFilter();
+
+      Uri result = filter.Apply(new Uri("HttPS://www.nancyfx.org"), null);
+      Assert.Equal("https", result.Scheme);
+    }
+  }
+}
---
+++ src/Nancy.Linker/EnsureHttpsFilter.cs
@@ -0,0 +1,24 @@
+﻿namespace Nancy.Linker
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+
+  public class EnsureHttpsFilter : UriFilter
+  {
+    public EnsureHttpsFilter(IUriFilter nextFilter = null) : 
+      base(nextFilter)
+    {
+    }
+
+    protected override Uri OnApply(Uri uri, NancyContext context)
+    {
+      if (string.Compare(uri.Scheme, "https", true) == 0) return uri;
+
+      UriBuilder builder = new UriBuilder(uri);
+      builder.Scheme = "https";
+      return builder.Uri;
+    }
+  }
+}
